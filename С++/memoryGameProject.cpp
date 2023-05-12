d288f23f6257bb94f73a2b61a7a52dba53b54c17
mellovvin <124766075+creepymello@users.noreply.github.com>
2023-05-12 18:16:03 +0400

    добавила некоторые проверки

--- С++/memoryGameProject.cpp
+++ С++/memoryGameProject.cpp
@@ -4,14 +4,11 @@
 
 using namespace std;
 
-// размер поля 
-void sizeFoo(int rows, int cols);
-
 // для перемешивания карт
 void shuffleCards(int cards[], int numCards);
 
 // для вывода игрового поля
-void showBoard(int cards[], bool flipped[], int rows, int cols,int ttime);
+void showBoard(const int cards[], const bool flipped[], int rows, int cols, int ttime);
 
 // выбор 
 void chooseFoo(int index1, int index2, bool* flipped);
@@ -20,13 +17,18 @@ void chooseFoo(int index1, int index2, bool* flipped);
 bool isMatch(int cards[], int index1, int index2);
 
 // для ожидания нажатия
-void keyPress();
+void keypress();
 
 
-
+// нужно еще добавить функцию которая открывает угаданные карты,
+// проверку находится ли введенная пользователем карта в диапазоне numCards
 int main()
 {
+    int numFlips = 0;
+    int numMatches = 0;
+    int startTime = time(NULL);
     int rows{}, cols{};
+
     while (rows < 2 || rows > 10) {
         cout << "enter number of rows (2-10): ";
         cin >> rows;
@@ -38,8 +40,8 @@ int main()
         cout << "enter number of cols (2-10): ";
         cin >> cols;
     }
-    // размер поля 
 
+    // размер поля 
 
     int numCards = rows * cols;
     int ttime{};
@@ -47,13 +49,66 @@ int main()
     else ttime = 10;
     int* cards = new int[numCards];
     bool* flipped = new bool[numCards];
-
+    const int nepTime = 5;
     // перемпешивание
     shuffleCards(cards, numCards);
 
-    // вывод поля
-    showBoard(cards, flipped, rows, cols,ttime);
+    showBoard(cards, flipped, rows, cols, ttime);
+    while (numMatches < numCards / 2)
+    {
+        int index1;
+        do
+        {
+            cout << "Choose first card (1 - " << numCards << "): ";
+            cin >> index1;
+            index1--;
+            break;
 
+
+        } while (flipped[index1]);
+
+        // аользователь выбирает вторую карту
+        int index2;
+        do
+        {
+            cout << "Choose second card(1 - " << numCards << ") : ";
+            cin >> index2;
+            index2--;
+            break;
+
+        } while (flipped[index2] || index1 == index2);
+
+        // переворачиваем выбранные карты
+        flipped[index1] = true;
+        flipped[index2] = true;
+        numFlips++;
+
+        if (isMatch(cards, index1, index2)) {
+            cout << endl << "Match!" << endl;
+            flipped[index1] = true;
+            flipped[index2] = true;
+
+            if (numMatches == numCards / 2) {
+                cout << "Congratulations, you won!" << endl;
+            }
+        }
+        else {
+            cout << endl << "No match." << endl;
+            Sleep(1000);
+            showBoard(cards, flipped, rows, cols, nepTime);
+            flipped[index1] = false;
+            flipped[index2] = false;
+        
+        }
+        keypress();
+    }
+    int endTime = time(NULL);
+    int timeee = endTime - startTime;
+    cout << "Congratulations! You won!" << endl;
+    cout << "Number of flips: " << numFlips << endl;
+    cout << "Time taken: " << timeee << " seconds" << endl;
+
+    keypress();
 
 
 
@@ -80,7 +135,7 @@ void shuffleCards(int cards[], int numCards)
 
 
 
-void showBoard(int cards[], bool flipped[], int rows, int cols, int ttime) {
+void showBoard(const int cards[], const bool flipped[], int rows, int cols, int ttime){
     int timeLeft = ttime;
     while (timeLeft > 0) {
         system("cls");
@@ -91,11 +146,14 @@ void showBoard(int cards[], bool flipped[], int rows, int cols, int ttime) {
                 if (flipped[index]) {
                     cout << cards[index] << '\t';
                 }
-
+                else {
+                    cout << "X\t";
+                }
             }
             cout << endl;
         }
-        cout << endl << "Look carefully at the numbers and try to remember their meanings.\nThe values will disappear after " 
+
+        cout << endl << "Look carefully at the numbers and try to remember their meanings.\nThe values will disappear after "
             << timeLeft << " seconds" << endl;
 
         Sleep(1000);
@@ -114,20 +172,20 @@ void showBoard(int cards[], bool flipped[], int rows, int cols, int ttime) {
 
 
 
-void chooseFoo(int index1, int index2, bool* flipped) {
-
-    while (flipped[index1]) {
-        cout << "choose first card: ";
-        cin >> index1;
-        index1--;
-    }
-
-    while (index2 == index1 || flipped[index2]) {
-        cout << "choose second card: ";
-        cin >> index2;
-        index2--;
-    }
-}
+//void chooseFoo(int index1, int index2, bool* flipped) {
+//
+//    while (flipped[index1]) {
+//        cout << "choose first card: ";
+//        cin >> index1;
+//        index1--;
+//    }
+//
+//    while (index2 == index1 || flipped[index2]) {
+//        cout << "choose second card: ";
+//        cin >> index2;
+//        index2--;
+//    }
+//}
 
 
 
@@ -139,3 +197,8 @@ bool isMatch(int cards[], int index1, int index2)
 
 
 
+void keypress() {
+    cout << "please click ENTER to continue: ";
+    cin.ignore();
+    cin.get();
+}
